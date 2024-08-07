0c347fa9ca3a724526380afddcb6d6521772114f
Swapnillp <nilpatil917@gmail.com>
2024-08-07 12:57:19 +0530

    add new file

---
+++ MultiT2.java
@@ -0,0 +1,25 @@
+//by using Thread 
+  class Process1 extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++){
+            System.out.println("Process 1:"+ i);
+        }
+    }
+  }
+  class Process2 extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++){
+            System.out.println("Process 2:"+ i);
+        }
+    }
+
+  }
+  class MultiT2{
+    public static void main(String args[]){
+        Process1 p1=new Process1();
+        Process2 p2=new Process2();
+        p1.start();
+        p2.start();
+
+    }
+  }
