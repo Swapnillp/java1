843b222b5ec905300aaa2a3fbccec131529770bc
Swapnillp <nilpatil917@gmail.com>
2024-08-08 19:57:26 +0530

    add new swings program

---
+++ swingEx1.java
@@ -0,0 +1,14 @@
+//swing
+import javax.swing.*;
+class swingEx1 extends JFrame{
+    swingEx1(String s1){
+        super(s1);
+    }
+    
+    public static void main(String args[]){
+        swingEx1 s1=new swingEx1("welcome to java");
+        s1.setVisible(true);
+        s1.setSize(500,500);
+        s1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
