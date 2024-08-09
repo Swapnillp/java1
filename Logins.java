84a7ead0ba4201975dccfc2b79db4032e5d1db74
Swapnillp <nilpatil917@gmail.com>
2024-08-09 11:03:24 +0530

    add new

--- Logins.java
+++ Logins.java
@@ -3,14 +3,17 @@ import javax.swing.*;
 class Logins extends JFrame{
     void setComponents(){
         JLabel k1=new JLabel("welcome");
+        JTextField k2=new JTextField();
         setLayout(null);
-        k1.setBounds(200,200,100,50);
+        k1.setBounds(100,100,100,50);
         add(k1);
+        k2.setBounds(100,50,50,20);
+        add(k2);
         
     }
     public static void main(String args[]){
         Logins j1=new Logins();
-        j1.setSize(500,500);
+        j1.setSize(800,800);
         j1.setVisible(true);
         j1.setComponents();
         j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
