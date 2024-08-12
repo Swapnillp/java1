5c8f7dd3d565e3588a672d483468b0ea605df2a9
Swapnillp <nilpatil917@gmail.com>
2024-08-12 17:24:40 +0530

    add program

---
+++ Arraylist1.java
@@ -0,0 +1,12 @@
+//Arraylist
+import java.util.*;
+class Arraylist1{
+    public static void main(String args[]){
+        ArrayList a1=new ArrayList();
+        a1.add("A");
+        a1.add("B");
+        a1.add("C");
+        a1.add("D");
+        System.out.println(a1);
+    }
+}
