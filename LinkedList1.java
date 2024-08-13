55680303a15a7b173f30896d0fa29e048b893e45
Swapnillp <nilpatil917@gmail.com>
2024-08-13 16:28:13 +0530

    linkedlist

---
+++ LinkedList1.java
@@ -0,0 +1,11 @@
+//LinkedList
+import java.util.*;
+class LinkedList1{
+    public static void main (String args[]){
+        LinkedList l1=new LinkedList();
+        l1.addFirst("Swapnil");
+        l1.addLast("Patil");
+        System.out.println(l1.getFirst());
+        System.out.println(l1.getLast());
+    }
+}
