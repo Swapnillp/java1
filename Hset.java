0c5ba03934636b2199c2c9ae22735fe12438ef09
Swapnillp <nilpatil917@gmail.com>
2024-08-13 17:26:29 +0530

    hashset

---
+++ Hset.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Hset{
+    public static void main(String args[]){
+        HashSet h1=new HashSet();
+        h1.add("nil");
+        h1.add("nakshu");
+        h1.add("karan");
+        h1.add("swapnil");
+        System.out.println(h1);
+    }
+}
